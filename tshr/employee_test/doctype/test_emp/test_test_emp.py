a97f17bc255cd2777cb5f39d166205c981fd68f7
Khush Ramani <khush.ramani@technovate.in>
2024-09-05 18:14:09 +0530

    Test Custom Changes

---
+++ tshr/employee_test/doctype/test_emp/test_test_emp.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Tushar Thakkar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTestEMP(FrappeTestCase):
+	pass
