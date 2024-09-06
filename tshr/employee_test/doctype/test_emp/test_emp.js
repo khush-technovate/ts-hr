afcb44f1d53a3e077c3a5d64bf872c3835b9122a
PrathamShah3011 <pratham3011@gmail.com>
2024-09-06 12:44:47 +0530

    test run

--- tshr/employee_test/doctype/test_emp/test_emp.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Tushar Thakkar and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Test EMP", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- tshr/employee_test/doctype/test_emp/test_test_emp.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Tushar Thakkar and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTestEMP(FrappeTestCase):
-	pass
